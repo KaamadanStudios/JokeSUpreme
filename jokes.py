92b8a48d3d933f25264514614f1d8d0c36802b65
KaamadanStudios <78262948+KaamadanStudios@users.noreply.github.com>
2021-01-30 14:17:12 +0100

    Added the module
    Put the archive "jokes.py" in the same folder, subfolder... than your project.

---
+++ jokes.py
@@ -0,0 +1,27 @@
+def joke_1():
+    print ("Why did the math book commit suicide? He had too much problems.")
+def joke_2():
+    print ("I clicked the pen nervously. Now this kid wants to know why I invaded Cuba.")
+def joke_3():
+    print ("You can't fart in an Apple Store. They don't have Windows.")
+def joke_4():
+    print ("Me: I need a new phone!")
+    print ("Google: Interesting.jpg")
+def joke_5():
+    print ("The No U box is good. But don't sit on it.")
+def joke_6():
+    print ("Joke loading... wait until we have one.")
+def joke_7():
+    print ("What do you call a snake whose length is 3.14 cm? A πthon.")
+def joke_8():
+    print ("I cried when dad chopped onions. Onions was a good dog.")
+def joke_9():
+    print ("If you were born in November 30th of 1999, congrats.")
+    print ("You will be 69 years, 4 months and 20 days old... ")
+    print ("on 4/20/69")
+def joke_10():
+    print ("These were the 10 horsemen of jokes.")
+def joke_11():
+    print ("I lied, there's another.")
+def propa_joke():
+    print ("Propa- Jokes given by Russia.")
